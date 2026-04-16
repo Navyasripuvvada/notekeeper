f97ad7c918508510768fb1a6bc0331b0e7c3e888
Navyasripuvvada <puvvadanavyasri5809@gmail.com>
2026-04-16 10:34:40 +0530

    adding root route

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,9 @@ app.use(express.json());
 app.use(cors({origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true}));
-
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
 app.use("/notes",routes)
 app.use("/auth",authroutes);
 export  default app;
