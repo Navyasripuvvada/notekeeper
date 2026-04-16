097075d393ae98704af504ab4f75dd5ef0bbc674
Navyasripuvvada <puvvadanavyasri5809@gmail.com>
2026-04-16 11:17:05 +0530

    add cors

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 const app = express();
 app.use(express.json());
-app.use(cors({origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
+app.use(cors({origin: "https://task-ryzer-nv-notekeeper.netlify.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true}));
 app.get("/", (req, res) => {
