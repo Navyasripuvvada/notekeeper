1d91170516c1e8c9e8b40adab5e1656e18294aa0
Navyasripuvvada <puvvadanavyasri5809@gmail.com>
2026-04-17 14:37:35 +0530

    updated cors url

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 const app = express();
 app.use(express.json());
-app.use(cors({origin: "https://task-ryzer-nv-notekeeper.netlify.app",
+app.use(cors({origin: "https://task-ryzer-nv-notekeeper.netlify.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true}));
 app.get("/", (req, res) => {
