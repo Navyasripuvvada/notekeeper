a30b672d2267153cdb3877bec65477f515a73ba6
Navyasripuvvada <puvvadanavyasri5809@gmail.com>
2026-04-17 13:50:59 +0530

    urlchange

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,15 @@
-import { Pool} from "pg";
+import { Pool } from "pg";
 import dotenv from "dotenv";
-dotenv.config();
-export const pool = new Pool({
-    user:process.env.DB_USER,
-    host:process.env.DB_HOST,
-    database:process.env.DB_NAME,
-    password:process.env.DB_PASSWORD,
-    port:Number(process.env.DB_PORT)
 
-})
-pool.connect()
-.then(()=>console.log("postgresql is connected succesfully"))
-.catch((err)=>console.log(err.message));
+dotenv.config();
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+pool.query("SELECT 1")
+  .then(() => console.log("PostgreSQL connected successfully"))
+  .catch((err) => console.log(err.message));
